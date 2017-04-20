9d1f559d779d3b35d0013c059b47160575395dbd
ptutak <p.tutak@outlook.com>
2017-04-20 10:07:37 +0200

    client

--- src/client/LoginWindowController.java
+++ src/client/LoginWindowController.java
@@ -1,8 +1,23 @@
 package client;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class LoginWindowController {
+	
+	@FXML private TextField loginTextField;
+	@FXML private TextField ipTextField;
+	@FXML private TextField portTextField;
+	@FXML private Button connectLoginButton;
+	@FXML private Label infoLabel;
+	
+	@FXML private void connectLoginButtonClick(){
+		String ip = ipTextField.getText();
+		String port = portTextField.getText();
+		String login=loginTextField.getText();
+	}
 	
 	@FXML public void initialize(){
 		
